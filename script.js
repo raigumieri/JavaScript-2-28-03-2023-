ab2f4b9e4d18fbf3925f65943c7c4cc1363d0f26
raigumieri <127215645+raigumieri@users.noreply.github.com>
2023-04-04 17:27:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
 //SUBSTITUIR TEXTO
 
-//let texto = "Estão chegando as provas"
-//console.log(texto.replace("provas","avaliações"))
+let texto = "Estão chegando as provas"
+console.log(texto.replace("provas","avaliações"))
 
 //FIXAR O VALOR
 
-//let num = 223.45555
-//console.log(num.toFixed(2))
+let num = 223.45555
+console.log(num.toFixed(2))
 
 //CAIXA ALERTA
-//alert("Olá meu amigo")
+alert("Olá meu amigo")
 
 //CAIXA DE CONFIRMAÇÃO
 
-//let teste =confirm("Você é um aluno??")
-//console.log("Teste")
+let teste =confirm("Você é um aluno??")
+console.log("Teste")
 
 
 //CAIXA DE PROMPT
-/*
+
 let texto1 = prompt("Digite seu nome")
 document.write("<br>")
 //console.log("O nome dele é:", texto1)
 document.write("O nome dele é: ", texto1)
-*/
+
 
 
 //IF
 
-/*
+
 if(20 >= 20){
     document.write("20 é maior")
     document.write("<br> <br>")
@@ -43,23 +43,23 @@ if(25 > 15){
     document.write("Número menor")
     document.write("<br> <br>")
 }
-*/
+
 
 
 //IF E ELSE DECLARANDO VÁRIAVEL
-/*
+
 let numero = 10
 if(numero > 10){
     document.write("O número é maior")
 }else{
     document.write("O número é menor")
     document.write("<br> <br>")
-*/
+
 
 
 //IF E ELSE ENCADEADO
 
-/*
+
     // let idade = 18
 
     let idade = prompt("Digite sua idade: ")
@@ -74,7 +74,7 @@ if(numero > 10){
         document.write("Um adulto experiente vulgo maduro")
     }
 }
-*/
+
 
 
  let idade = prompt ("Digite sua idade: ")
